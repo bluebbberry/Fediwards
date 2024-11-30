007a3ea9b6142d1e9d2b70fa85fe703a147f0789
blueberry <jnb@campus.uni-paderborn.de>
2024-11-30 21:10:21 +0100

    feat: add start page

---
+++ frontend/src/app/start-page/start-page.component.ts
@@ -0,0 +1,12 @@
+// start-page.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-page',
+  templateUrl: './start-page.component.html',
+  styleUrls: ['./start-page.component.scss'],
+  standalone: true,
+  imports: []
+})
+export class StartPageComponent {
+}
