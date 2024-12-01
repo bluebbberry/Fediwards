a563252f51fcd975d6e2bd0bdc3c50b347105af9
blueberry <jnb@campus.uni-paderborn.de>
2024-12-01 11:51:06 +0100

    feat: replace env variables with command line variables

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 import { createRestAPIClient } from "masto";
 
+const args = process.argv.slice(2);
+if (args.length < 2) {
+  console.error('Expected at least two arguments!');
+  process.exit(1);
+}
+
+const URL = args[0];
+const TOKEN = args[1];
+
 const masto = createRestAPIClient({
-  url: process.env.URL,
-  accessToken: process.env.TOKEN,
+  url: URL,
+  accessToken: TOKEN,
 });
 
 const status = await masto.v1.statuses.create({
