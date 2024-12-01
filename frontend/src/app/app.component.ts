6b168caa0d765e99a9665be95a7457bee96dd9f8
blueberry <jnb@campus.uni-paderborn.de>
2024-12-01 12:33:12 +0100

    feat: implement communication between frontend and backend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { StartPageComponent } from './start-page/start-page.component'
+import { StartPageComponent } from './start-page/start-page.component';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,11 @@ import { StartPageComponent } from './start-page/start-page.component'
 export class AppComponent {
   title = 'frontend';
 
+  constructor(private http: HttpClient) {}
+
   sendToMyAccount() {
     console.log("Clicked on send");
+    this.http
+      .get("http://localhost:3000/status").subscribe((response: any) => console.log("Success"));
   }
 }
