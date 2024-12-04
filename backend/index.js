a83f9f7b76b8762edc6a5f91857330fd36044234
blueberry <jnb@campus.uni-paderborn.de>
2024-12-04 20:49:31 +0100

    feat: jea was not correctly parsing delay

--- backend/index.js
+++ backend/index.js
@@ -37,8 +37,8 @@ async function sendMsgToServer(message, sidekick) {
         await send(message);
     } else if (sidekick === 'jea') {
         const msgSplit = splitAfterHash(message);
-        if (msgSplit.length > 1) {
-            const noOfMinutes = msgSplit[0];
+        if (msgSplit.length > 1 && isNumeric(msgSplit[0].substring(1))) {
+            const noOfMinutes = Number(msgSplit[0].substring(1));
             const restOfMessage = msgSplit[1];
             setTimeout(() => send(restOfMessage), 1000 * noOfMinutes * 60);
         } else {
@@ -47,6 +47,13 @@ async function sendMsgToServer(message, sidekick) {
     } else {
         await send(message);
     }
+}
+
+// from https://stackoverflow.com/questions/175739/how-can-i-check-if-a-string-is-a-valid-number
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+        !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
 }
 
 async function send(message, sidekick) {
